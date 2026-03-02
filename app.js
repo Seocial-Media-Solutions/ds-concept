c0b3c37dfcb933bb654dc16bbc9bf347e8c70786
Dilkhush4707 <155545584+Dilkhush4707@users.noreply.github.com>
2026-03-02 11:19:46 +0530

    backend  polish

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // server.js
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require('cors');
 
 const app = express();
-// const uri = 'mongodb+srv://jeetendrasinghrathore1:B2uqhdlmpbcFv8B4@cluster1.zahwy.mongodb.net/?retryWrites=true&w=majority&appName=cluster1'
-const uri = "mongodb://localhost:27017/architecture_db"
-// MongoDB Connection - removed deprecated options
-mongoose.connect(uri)
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log('MongoDB connection error:', err));
 
