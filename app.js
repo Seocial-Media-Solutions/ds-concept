4b688a113493a9c33469776b8f3643e413009037
Dilkhush4707 <155545584+Dilkhush4707@users.noreply.github.com>
2026-03-05 15:32:33 +0530

    run

--- app.js
+++ app.js
@@ -24,4 +24,4 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/projects', require('./routes/projectRoutes'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
